0e449d780b8a7ca1138b66f44546ec838764b8e4
Marcus Wysocki <45272662+MarcusWysocki@users.noreply.github.com>
2021-11-24 16:18:08 +0000

    Fully generates a new game. Just need to change to 'begin' path.

--- src/main/java/com/MasterMind/data/GameInMemDao.java
+++ src/main/java/com/MasterMind/data/GameInMemDao.java
@@ -1,14 +1,21 @@
 package com.MasterMind.data;
 
 import com.MasterMind.Models.Game;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Random;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GameInMemDao implements GameDao {
 
+
+    static DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
     private static final List<Game> games = new ArrayList<>();
 
     @Override
@@ -20,6 +27,14 @@ public class GameInMemDao implements GameDao {
                 .orElse(0) + 1;
 
         game.setId(nextId);
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        String formattedTime = dateTime.format(formatter);
+        game.setTimeStarted(formattedTime);
+
+        int[] answer = generateAns();
+        game.setAnswer(answer);
+
         games.add(game);
         return game;
 
@@ -57,4 +72,28 @@ public class GameInMemDao implements GameDao {
     public boolean deleteById(int id) {
         return games.removeIf(i -> i.getId() == id);
     }
+
+    private int[] generateAns() {
+        int[] used = {0,0,0,0,0,0,0,0,0,0};
+        int[] ans = {0,0,0,0};
+
+        for (int i = 0; i < 4; i++) {
+            ans[i] = randomNum(used);
+            used[ans[i]] = 1;
+        }
+
+        return ans;
+    }
+
+    private int randomNum(int[] used) {
+        Random rand = new Random();
+        int num = rand.nextInt(10);
+
+        if(used[num] == 1) {
+            return randomNum(used);
+        } else {
+            return num;
+        }
+    }
+
 }
